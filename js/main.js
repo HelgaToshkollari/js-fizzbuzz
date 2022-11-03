3ccde32907990ea194e3db5f9716a1d4b859ccbb
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-03 18:22:42 +0100

    adds changes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
+let result = document.getElementById("result");
+
+
 for ( let i = 1; i <= 100; i++) {
-    
-    let number = i;
 
-    if ((i % 3 == 0) && (i % 5 == 0)) {
-        number = "FizzBuzz";
-    } else if (i % 3 == 0) {
-        number = "Fizz";
-    } else if (i % 5 == 0) {
-        number = "Buzz"; 
-        
+    let square = document.createElement("div");
+
+    square.classList.add("square");
+
+    result.append(square);
+
+    if ( i % 3 === 0 && i % 5 === 0 ){
+        square.classList.add("fizzBuzz");
+        square.append("fizzBuzz");
+
+    } else if ( i % 5 === 0 ) {
+        square.classList.add("buzz");
+        square.append("buzz");
+
+
+    } else if ( i % 3 === 0 ) {
+        square.classList.add("fizz");
+        square.append("fizz");
+    } else {
+        result.append(square);
+        square.append(i);
     }
-
-    console.log(number);
-    document.getElementById("result").innerHTML +="<span>" + number + "</span>";
+    
+   
     
 }
